a9b7d71bdbd4e5942a09f2e1300ecffa437afef9
ahmed nur <ahmedlearn01@gmail.com>
2023-08-13 16:32:42 +0300

    map with makers working well & on process of calculation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
   Box,
   Button,
@@ -9,19 +10,61 @@ import {
   Text,
 } from '@chakra-ui/react'
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import { useJsApiLoader,GoogleMap, Marker, Autocomplete } from '@react-google-maps/api'
+import { useRef, useState } from 'react'
+import { LoadScript } from '@react-google-maps/api'
+
+const center = {lat: 2.030882936228783,lng: 45.302110411366066}
+// 2.030882936228783, 45.302110411366066
 
 function App() {
+  const [map, setMap] = useState(/** @type google.maps.Map */ null)
+  const [directionRes, setDirectionRes] = useState(null)
+  const [duration, setDuration] = useState('')
+  const [distance, setDistance] = useState('')
+
+  const originRef = useRef();
+  const distinationRef = useRef()
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_KEY,
+    libraries: ['places']
+  })
+
+  if(!isLoaded){
+    
+    return <h1>Loading....</h1>
+  }
+
+  // function to calculate distance and duration
+
+  const calcMap = async () => {
+    if(originRef.current.value === '' || distinationRef.current.value === ''){
+      return
+    } else {
+
+    }
+
+  }
+  
   return (
     <Flex
       position='relative'
       flexDirection='column'
       alignItems='center'
-      bgColor='blue.200'
       h='100vh'
       w='100vw'
     >
       <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
-
+      <GoogleMap 
+      center={center}
+      zoom={15}
+      mapContainerStyle={{width: '100%', height: '100%'}}
+      onLoad={map => setMap(map)}
+      >
+        {/* onPositionChanged property is available??? */}
+        <Marker position={center}/> 
+      </GoogleMap>
       <Box
         p={4}
         borderRadius='lg'
@@ -32,8 +75,12 @@ function App() {
         zIndex='modal'
       >
         <HStack spacing={4}>
-          <Input type='text' placeholder='Origin' />
-          <Input type='text' placeholder='Destination' />
+          <Autocomplete>
+          <Input type='text' placeholder='Origin' ref={originRef}/>
+          </Autocomplete>
+          <Autocomplete>
+          <Input type='text' placeholder='Destination' ref={distinationRef}/>
+          </Autocomplete>
           <ButtonGroup>
             <Button colorScheme='pink' type='submit'>
               Calculate Route
@@ -52,7 +99,7 @@ function App() {
             aria-label='center back'
             icon={<FaLocationArrow />}
             isRound
-            onClick={() => alert(123)}
+            onClick={() => map.panTo(center)}
           />
         </HStack>
       </Box>
